ee2546a4d684d752511e7df102677623f1adf5d6
Tanjin Adnan Abir <tanjinadnanabir@gmail.com>
2025-10-06 15:29:51 +0600

    base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chicken_disease_classification import logger
 from chicken_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chicken_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +10,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/chicken_disease_classification/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,16 @@
+from chicken_disease_classification.config.configuration import ConfigurationManager
+from chicken_disease_classification.components.prepare_base_model import PrepareBaseModel
+from chicken_disease_classification import logger
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
